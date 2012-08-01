066b0cb3b9253469463256bf499d1649ce04670b
Goutte <antoine.goutenoir@gmail.com>
2012-08-01 04:47:55 +0200

    Moving webocracy factories in appropriate folder Removing usage of deprecated Factory feature

---
+++ spec/factories/webocracy.rb
@@ -0,0 +1,33 @@
+#   Copyright (c) 2010-2011, Diaspora In  This file is
+#   licensed under the Affero General Public License version 3 or late  See
+#   the COPYRIGHT file.
+
+#For Guidance
+#http://github.com/thoughtbot/factory_girl
+# http://railscasts.com/episodes/158-factories-not-fixtures
+
+FactoryGirl.define do
+
+  factory :webocracy_generic_proposition, :class => Webocracy::GenericProposition do
+    sequence(:text) { |n| "Generic Propal #{n}" }
+    association :author, :factory => :person
+    after_build do |prop|
+      prop.diaspora_handle = prop.author.diaspora_handle
+    end
+  end
+
+  factory :yes_no_maybe_proposition, :class => Webocracy::YesNoMaybeProposition do
+    sequence(:text) { |n| "YNM Propal #{n}" }
+    association :author, :factory => :person
+    after_build do |prop|
+      prop.diaspora_handle = prop.author.diaspora_handle
+    end
+  end
+
+  #factory :decision do
+  #  association :author, :factory => :person
+  #  association :target, :factory => :proposition
+  #end
+
+
+end
--- spec/lib/webocracy/pollable_spec.rb
+++ spec/lib/webocracy/pollable_spec.rb
@@ -8,65 +8,65 @@ describe Webocracy::AbstractPollable do
   before do
 
     # GenericProposition is GenericPollable
-    @genericPollable = Factory(:webocracy_generic_proposition, :author => alice.person)
+    @generic_pollable = FactoryGirl.build(:webocracy_generic_proposition, :author => alice.person)
 
   end
 
   describe '#get_sum' do
     it 'initially is 0 (without decisions)' do
-      @genericPollable.get_sum.should == 0
+      @generic_pollable.get_sum.should == 0
     end
     it 'has the value of the decision if there is only one' do
       d = Webocracy::Decision.new({ :value => 1 })
-      @genericPollable << d
-      @genericPollable.get_sum.should == 1
+      @generic_pollable << d
+      @generic_pollable.get_sum.should == 1
     end
     it 'holds the sum of the values of the decisions' do
       d1 = Webocracy::Decision.new({ :value => 1 })
       d2 = Webocracy::Decision.new({ :value => 2 })
       d3 = Webocracy::Decision.new({ :value => 3 })
-      @genericPollable << d1
-      @genericPollable << d2
-      @genericPollable << d3
-      @genericPollable.get_sum.should == 6
+      @generic_pollable << d1
+      @generic_pollable << d2
+      @generic_pollable << d3
+      @generic_pollable.get_sum.should == 6
     end
     it 'works with negative decision values' do
       d1 = Webocracy::Decision.new({ :value => 1 })
       d2 = Webocracy::Decision.new({ :value => 1 })
       d3 = Webocracy::Decision.new({ :value => -3 })
-      @genericPollable << d1
-      @genericPollable << d2
-      @genericPollable << d3
-      @genericPollable.get_sum.should == -1
+      @generic_pollable << d1
+      @generic_pollable << d2
+      @generic_pollable << d3
+      @generic_pollable.get_sum.should == -1
     end
   end
 
   describe '#get_mean' do
     it 'initially is 0 (without decisions)' do
-      @genericPollable.get_mean.should == 0
+      @generic_pollable.get_mean.should == 0
     end
     it 'has the value of the decision if there is only one' do
       d = Webocracy::Decision.new({ :value => 42 })
-      @genericPollable << d
-      @genericPollable.get_mean.should == 42
+      @generic_pollable << d
+      @generic_pollable.get_mean.should == 42
     end
     it 'holds the mean value of the values of the decisions' do
       d1 = Webocracy::Decision.new({ :value => 1 })
       d2 = Webocracy::Decision.new({ :value => 2 })
       d3 = Webocracy::Decision.new({ :value => 3 })
-      @genericPollable << d1
-      @genericPollable << d2
-      @genericPollable << d3
-      @genericPollable.get_mean.should == 2
+      @generic_pollable << d1
+      @generic_pollable << d2
+      @generic_pollable << d3
+      @generic_pollable.get_mean.should == 2
     end
     it 'works with negative decision values' do
       d1 = Webocracy::Decision.new({ :value => -5 })
       d2 = Webocracy::Decision.new({ :value => -25 })
       d3 = Webocracy::Decision.new({ :value => -30 })
-      @genericPollable << d1
-      @genericPollable << d2
-      @genericPollable << d3
-      @genericPollable.get_mean.should == -20
+      @generic_pollable << d1
+      @generic_pollable << d2
+      @generic_pollable << d3
+      @generic_pollable.get_mean.should == -20
     end
   end
 
