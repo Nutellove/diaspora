c4c91e9a68db8fcd369dfc8212f227c707fa6fec
Goutte <antoine.goutenoir@gmail.com>
2012-08-01 10:24:54 +0200

    Some love for specs-driven dev <3

--- spec/lib/webocracy/yes_no_maybe_pollable_spec.rb
+++ spec/lib/webocracy/yes_no_maybe_pollable_spec.rb
@@ -28,5 +28,24 @@ module Webocracy
       end
     end
 
+    describe '#get_winner' do
+      it 'should return 0 initially' do
+        [1,0,-1].each { |v| @ynm_pollable << new_decision(v) }
+        @ynm_pollable.get_winner.should == 0
+      end
+      it 'should return the value for a single Decision' do
+        [1].each { |v| @ynm_pollable << new_decision(v) }
+        @ynm_pollable.get_winner.should == 1
+      end
+      it 'should return a value in {-1,0,1}' do
+        [1,1,0,-1].each { |v| @ynm_pollable << new_decision(v) }
+        @ynm_pollable.get_winner.should == 1
+      end
+      it 'should return 0 in case of a draw' do
+        [1,-1].each { |v| @ynm_pollable << new_decision(v) }
+        @ynm_pollable.get_winner.should == 0
+      end
+    end
+
   end
 end
