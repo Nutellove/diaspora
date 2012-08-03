d6c98d3532145051cea86aa7b1011118289acd0e
Goutte <antoine.goutenoir@gmail.com>
2012-08-03 12:00:39 +0200

    passing specs for delegation

--- spec/lib/webocracy/delegation_spec.rb
+++ spec/lib/webocracy/delegation_spec.rb
@@ -11,41 +11,29 @@ module Webocracy
     before do
       extend HelperMethods
 
-      #@politics = FactoryGirl.build(:aspect, { :name => 'Politics', :user => alice })
-
       # create aspect 'Politics'
       @politics = alice.aspects.create(:name => 'Politics') # I18n.t('aspects.seed.politics')
-      alice.add_contact_to_aspect(alice.contact_for(bob.person), @politics)
+      #@politics = FactoryGirl.build(:aspect, { :name => 'Politics', :user => alice })
+      #alice.add_contact_to_aspect(alice.contact_for(bob.person), @politics)
 
       @proposition = FactoryGirl.build(:webocracy_yes_no_maybe_proposition, :author => eve.person)
 
     end
 
-    describe 'Aspects' do
-      it "should find bob in alice's delegates" do
-        alice.delegates.include?(bob.person).should be_true
-      end
+    describe 'Delegation' do
       it "should not find eve in alice's delegates" do
         alice.delegates.include?(eve.person).should be_false
       end
-      it "should find eve in alice's delegates after we add her" do
-        pending "thinking about user aspects, maybe we need system aspects"
+      it "should find eve in alice's delegates after we add a Delegation" do
+        d = FactoryGirl.build(:webocracy_delegation, { :user => alice, :person => eve.person })
+        alice.delegations << d
+        alice.delegates.include?(eve.person).should be_true
+      end
+      it "should find eve in alice's delegates after we add a Person" do
         alice.delegates << eve.person
         alice.delegates.include?(eve.person).should be_true
       end
     end
-
-    describe 'Delegation' do
-
-      it 'test' do
-
-        d = FactoryGirl.build(:webocracy_delegation, { :user => alice, :person => eve.person })
-        alice.delegations << d
-
-      end
-
-    end
-
 
   end
 end
