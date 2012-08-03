399c7dc5c5e189661a5789e1fab7a6d3a21ac170
Goutte <antoine.goutenoir@gmail.com>
2012-08-03 15:06:16 +0200

    Revoking a delegation specs

--- spec/lib/webocracy/delegation_spec.rb
+++ spec/lib/webocracy/delegation_spec.rb
@@ -7,7 +7,7 @@ require 'spec_helper'
 
 module Webocracy
 
-  describe 'Delegation (fixme)' do
+  describe 'Delegation' do
     before do
       extend HelperMethods
 
@@ -20,28 +20,56 @@ module Webocracy
 
     end
 
-    describe 'Delegation' do
-      it "should not find eve in alice's delegates" do
-        alice.delegates.include?(eve.person).should be_false
+    describe 'Basic Operations' do
+
+      describe 'Adding' do
+
+        it "should not find eve in alice's delegates" do
+          alice.delegates.include?(eve.person).should be_false
+        end
+        it "should find eve in alice's delegates after we add a Delegation" do
+          delegation = alice.delegations.create(:person => eve.person)
+          alice.delegates.include?(eve.person).should be_true
+        end
+        it "should ignore foreign delegations" do
+          d = FactoryGirl.build(:webocracy_delegation, { :user => bob, :person => eve.person })
+          alice.delegations << d
+          alice.delegates.include?(eve.person).should be_false
+        end
+        it "should find eve in alice's delegates after we add her in Person" do
+          alice.delegates << eve.person
+          alice.delegates.include?(eve.person).should be_true
+          alice.delegations.find_all{ |d| d.person == eve.person }.length.should == 1
+        end
+        it 'does not allow you to target yourself' do
+          delegation = alice.delegations.create(:person => alice.person)
+          delegation.should have(1).error_on(:person_id)
+        end
+
       end
-      it "should find eve in alice's delegates after we add a Delegation" do
-        delegation = alice.delegations.create(:person => eve.person)
-        alice.delegates.include?(eve.person).should be_true
+
+      describe 'Revoking' do
+
+        it 'should work, using delegations' do
+          @delegation = alice.delegations.create(:person => eve.person)
+          alice.delegations.size.should == 1
+          alice.delegates.size.should   == 1
+          alice.delegations.delete @delegation
+          alice.delegations.size.should == 0
+          alice.delegates.size.should   == 0
+        end
+
+        it 'should work, using delegates' do
+          alice.delegates << eve.person
+          alice.delegations.size.should == 1
+          alice.delegates.size.should   == 1
+          alice.delegates.delete eve.person
+          alice.delegations.size.should == 0
+          alice.delegates.size.should   == 0
+        end
+
       end
-      it "should ignore foreign delegations" do
-        d = FactoryGirl.build(:webocracy_delegation, { :user => bob, :person => eve.person })
-        alice.delegations << d
-        alice.delegates.include?(eve.person).should be_false
-      end
-      it "should find eve in alice's delegates after we add a Person" do
-        alice.delegates << eve.person
-        alice.delegates.include?(eve.person).should be_true
-        alice.delegations.find_all{ |d| d.person == eve.person }.length.should == 1
-      end
-      it 'does not allow you to target yourself' do
-        delegation = alice.delegations.create(:person => alice.person)
-        delegation.should have(1).error_on(:person_id)
-      end
+
     end
 
   end
