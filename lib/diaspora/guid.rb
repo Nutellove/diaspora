f4a9e278c746c411b9cdd11182f8cc6d2e117106
Goutte <antoine.goutenoir@gmail.com>
2012-08-17 08:24:40 +0200

    Cleanup

--- lib/diaspora/guid.rb
+++ lib/diaspora/guid.rb
@@ -1,17 +1,21 @@
-#implicitly requires roxml
+#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
 
+# Creates a before_create callback which calls #set_guid and makes the guid serialize in to_xml
+# implicitly requires roxml
 module Diaspora::Guid
-  # Creates a before_create callback which calls #set_guid and makes the guid serialize in to_xml
   def self.included(model)
     model.class_eval do
+
       before_create :set_guid
-      xml_attr :guid
+      xml_attr  :guid
       validates :guid, :uniqueness => true
 
     end
   end
 
-  # @return [String] The model's guid.
+  # @return String The model's guid.
   def set_guid
     self.guid = SecureRandom.hex(8) if self.guid.blank?
   end
